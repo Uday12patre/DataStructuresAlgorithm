74ba69e95796fd4f34c33da6d0dd3c46c94ab39d
Uday12patre <uday12patre.code@gmail.com>
2025-09-23 13:59:59 +0000

    enQueue Works good but deQueue Yet to write.

---
+++ Assignment/PriorityQ.cpp
@@ -0,0 +1,112 @@
+#include<cstdlib>
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+
+class PriorityQueue
+{
+	private:
+    	int *arr, size, n, rear;
+   	 
+    	void swap(int i, int j)
+    	{
+        	int temp = arr[i];
+        	arr[i] = arr[j];
+        	arr[j] = temp;
+    	}
+   	 
+    	void Adjust()
+    	{
+        	int i = rear;
+        	int p_i = (i-1)/2;
+        	while(i!=0)
+        	{
+            	if(arr[i] > arr[p_i])
+            	{
+                	swap(i, p_i);
+                	i = p_i;
+                	p_i = (i-1)/2;
+            	}
+            	else
+                	return;
+        	}
+    	}
+	public:
+    	PriorityQueue()
+    	{
+            	rear = -1;
+            	size = 100;
+            	arr = new int[size];
+    	}
+   	 
+    	int IsEmpty()
+    	{
+        	if(rear == 0)
+            	return 1;
+        	return 0;
+    	}
+   	 
+    	int IsFull()
+    	{
+        	if(rear >= size)
+            	return 1;
+        	return 0;
+    	}
+   	 
+    	void enQueue(int n)
+    	{
+        	if(IsFull())
+            	return;
+       	 
+        	arr[++rear] = n;
+        	Adjust();
+    	}
+   	 
+    	int peek()
+    	{
+        	if(IsEmpty())
+            	return INT_MIN;
+        	return arr[0];
+    	}
+   	 
+    	void Display()
+    	{
+        	for(int i = 0; i <= rear; i++)
+        	{
+            	cout << arr[i] <<"\t";
+        	}
+        	cout << endl;
+       	 
+        	return;
+    	}
+   	 
+    	void deQueue()
+    	{
+        	int K = peek();
+        	arr[0] = arr[rear--];
+        	Adjust();
+        	cout  << K;
+        	return;
+    	}
+};
+
+int main()
+{
+	PriorityQueue Q;
+	for(int i = 0; i < 10; i++)
+	{
+    	int n = (rand() % 100) + 1;
+    	cout << n << "\t";
+    	Q.enQueue(n);
+    	cout << endl;
+    	Q.Display();
+    	cout << endl;
+	}
+    
+	Q.deQueue();
+	cout << endl;
+	Q.Display();
+	return 0;
+}
+
